f8bdb36a21bc51e4abb14af9025c948e159a579b
Vadim <snooperv@gmail.com>
2022-03-28 15:56:53 +0500

    Задание по базовому синтаксису

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,28 @@
 export function romanToInteger(str) {
+    var ar = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+    var rom = [
+        'I',
+        'IV',
+        'V',
+        'IX',
+        'X',
+        'XL',
+        'L',
+        'XC',
+        'C',
+        'CD',
+        'D',
+        'CM',
+        'M',
+    ];
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    let k = 0;
+    let n = ar.length - 1;
+    while (n >= 0 && k < str.length) {
+        if (str.substr(k, rom[n].length) == rom[n]) {
+            result += ar[n];
+            k += rom[n].length;
+        } else n--;
+    }
     return result;
 }
